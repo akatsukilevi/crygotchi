6fbc1ff71cc6e921cc351ee05d8d11cd2c73bc72
Akatsuki Levi <akatsukilevi@yahoo.co.jp>
2023-05-30 21:36:35 -0300

    fix: Added sprinting (Wacky animations)

--- components/hub/scripts/CryController.cs
+++ components/hub/scripts/CryController.cs
@@ -12,7 +12,9 @@ public partial class CryController : CharacterBody3D
     [Export] public float Gravity = -80.0f;
 
     private AnimationPlayer _animator;
+    private bool _isRunning = false;
     private Vector3 _moveDirection;
+    private Vector3 _inputDirection;
 
     public override void _Ready()
     {
@@ -20,13 +22,16 @@ public partial class CryController : CharacterBody3D
         this._animator.Play("Idle");
     }
 
+    public override void _Process(double delta)
+    {
+        this._inputDirection = GetInputDirection();
+        this._isRunning = Input.IsActionPressed("cry_sprint");
+    }
     public override void _PhysicsProcess(double delta)
     {
-        Vector3 inputDirection = GetInputDirection();
-
         // only forward direction if player trying to move forward or right
-        Vector3 forwards = this.Camera.GlobalTransform.Basis.Z * inputDirection.Z;
-        Vector3 right = this.Camera.GlobalTransform.Basis.X * inputDirection.X;
+        Vector3 forwards = this.Camera.GlobalTransform.Basis.Z * this._inputDirection.Z;
+        Vector3 right = this.Camera.GlobalTransform.Basis.X * this._inputDirection.X;
 
         // Get Move Direction relative to the camera
         this._moveDirection = forwards + right;
@@ -41,7 +46,7 @@ public partial class CryController : CharacterBody3D
         {
             //* If moving, look towards it
             this.LookAt(this.GlobalTransform.Origin + (-this._moveDirection), Vector3.Up);
-            this._animator.Play("Walking");
+            this._animator.Play(this._isRunning ? "Running" : "Walking");
             return;
         }
 
@@ -64,6 +69,8 @@ public partial class CryController : CharacterBody3D
 
         //* Clamp the velocity down
         if (velocity.Length() > this.MaxSpeed) velocity = velocity.Normalized() * this.MaxSpeed;
+
+        if (this._isRunning) velocity *= 5f;
 
         //* Add gravity force and return it
         velocity.Y = velocity.Y + this.Gravity * delta;
